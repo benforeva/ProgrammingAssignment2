974849e71c39dc99ca36bd9c371108cb5affe7db
Andre Dickson <andrebcdickson@gmail.com>
2017-09-11 17:03:38 -0400

    Completed code for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## When used together the following functions compute and cache 
+## the inverse of a given matrix. 
+## Usage: If cx = makeCacheMatrix(x) then cacheSolve(cx) will
+## always return x inverse but compute it only once.
 
-## Write a short comment describing this function
+## Returns a stateful wrapper (object) for x that lets the inverse 
+## of x be stored and retrieved.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    
+    store.matrix <- function(y) {
+            x <<- y
+            inv <<- NULL
+    }
+    unpack.matrix <- function() x
+    
+    store.inv <- function(inverse) inv <<- inverse
+    unpack.inv <- function() inv
+    list(store.matrix = store.matrix,
+         unpack.matrix = unpack.matrix,
+         store.inv = store.inv,
+         unpack.inv = unpack.inv)
 }
 
+## Returns the inverse of cx, only when cx = makeCacheMatrix(x). 
+## Computes and caches the inverse of x when first called returns
+## cached inverse on subsequent calls to cacheSolve.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cx, ...) {
+    ## Return a matrix that is the inverse of 'cx'
+    if(!is.null(cx$unpack.inv())) {
+        message(paste("getting cached inverse for",
+                      deparse(substitute(cx))))
+        return(cx$unpack.inv())
+    }
+    x.inv <- solve(cx$unpack.matrix(), ...)
+    cx$store.inv(x.inv)
+    x.inv
 }
